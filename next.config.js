3b2b5d97a221dbb279d4807ddcebdba5566df65b
haru-0205 <rm23081b@osaka-pct.ac.jp>
2024-06-04 14:17:12 +0900

    build(deps): delete 1 file and update 3 files

--- next.config.js
+++
@@ -1,9 +0,0 @@
-const ts = require('typescript')
-const withMDX = require('@next/mdx')()
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    pageExtensions: ['tsx', 'ts', 'jsx', 'js', 'mdx'],
-    output: "export",
-};
-module.exports =  withMDX(nextConfig);
