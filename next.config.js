88ae61a5d4b759d933361b9d64bbea5376380341
haru-0205 <rm23081b@osaka-pct.ac.jp>
2024-06-04 12:38:13 +0900

    update NextConfig

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const withMDX = require('@next/mdx')()
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    pageExtensions: ['tsx', 'ts', 'jsx', 'js', 'mdx']
+    pageExtensions: ['tsx', 'ts', 'jsx', 'js', 'mdx'],
+    output: "export",
 };
 module.exports =  withMDX(nextConfig);
